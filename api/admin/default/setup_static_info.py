2c57c14e103385d5b5f63afbb1fdc791ed297b64
Seggi SMS <seggimarugira@gmail.com>
2022-09-15 00:23:14 +0200

    add some changes

--- api/admin/default/setup_static_info.py
+++ api/admin/default/setup_static_info.py
@@ -2,7 +2,7 @@ from this import s
 from flask import Blueprint
 from flask import request
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from api.database.model_marsh import StudentAddressSchema, StudentParentSchema, StudentsSchema, UserSchema
+from api.database.model_marsh import ClassesSchema, StudentAddressSchema, StudentParentSchema, StudentsSchema, UserSchema
 from api.database.models import Classes, StudentAddress, StudentParent, Students, User
 
 from ... import db
@@ -11,6 +11,8 @@ from api.utils import responses as resp
 
 setup_static_info = Blueprint("setup_static_info", __name__,
                               url_prefix="/api/user/admin")
+
+class_schema = ClassesSchema()
 
 
 @setup_static_info.post('/setup-classes')
@@ -21,7 +23,7 @@ def setup_classes():
         request_data = request.json
 
         school_class = db.session.query(Classes).filter(
-            Classes.school_id == school_id).first()
+            Classes.name == request_data["name"]).first()
 
         if request_data['name'] is None:
             return response_with(resp.INVALID_INPUT_422)
@@ -31,7 +33,8 @@ def setup_classes():
 
         new_data = {
             "school_id": school_id,
-            "name": request_data["name"]
+            "name": request_data["name"],
+            "level_id": request_data["level_id"]
         }
 
         school_classes = Classes(**new_data)
@@ -43,3 +46,23 @@ def setup_classes():
     except Exception as e:
         print(e)
         return response_with(resp.INVALID_INPUT_422)
+
+
+@setup_static_info.get('/retrieve-classes')
+@jwt_required(refresh=True)
+def retrieve_classes():
+    school_id = get_jwt_identity()['id']
+    school_data = []
+    try:
+        school_default_data = db.session.query(Classes.name).filter(
+            Classes.school_id == school_id).all()
+
+        for school_class in school_default_data:
+            school_data.append({
+                **class_schema.dump(school_class),
+            })
+
+        return Response.success(message="success", data=school_data)
+
+    except Exception as e:
+        return response_with(resp.BAD_REQUEST_400)
