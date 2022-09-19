c29cd3e12b26184739ca63bd56cde9b878d02e9f
Seggi SMS <seggimarugira@gmail.com>
2022-09-19 16:48:04 +0200

    add some changes

--- api/admin/default/other_view.py
+++ api/admin/default/other_view.py
@@ -2,8 +2,8 @@ from this import s
 from flask import Blueprint
 from flask import request
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from api.database.model_marsh import ClassesSchema, CurrencySchema, LanguageSchema
-from api.database.models import Classes, Currency, Language
+from api.database.model_marsh import ClassesSchema, CurrencySchema, LanguageSchema, ModulesSchema
+from api.database.models import Classes, Currency, Language, Modules
 from ... import db
 from api.utils.responses import Response, response_with
 from api.utils import responses as resp
@@ -13,6 +13,7 @@ default_data = Blueprint("default_data", __name__,
 
 language_schema = LanguageSchema()
 currency_schema = CurrencySchema()
+module_schema = ModulesSchema()
 
 
 @default_data.post('/add-language')
@@ -45,11 +46,50 @@ def add_languages():
         return response_with(resp.INVALID_INPUT_422)
 
 
-@default_data.get('/retrieve-language')
+@default_data.post('/add-module')
 @jwt_required(refresh=True)
-def retrieve_languages():
+def add_module():
+    try:
+        request_data = request.json
+
+        module = db.session.query(Modules).filter(
+            Modules.name == request_data["name"]).first()
+
+        if request_data['name'] is None:
+            return response_with(resp.INVALID_INPUT_422)
+
+        if module:
+            return Response.success(message="Module already added.")
+
+        new_data = {
+            "name": request_data["name"],
+        }
+
+        modules = Modules(**new_data)
+        db.session.add(modules)
+        db.session.commit()
+
+        return Response.created(message="Module added with success.")
+
+    except Exception as e:
+        print(e)
+        return response_with(resp.INVALID_INPUT_422)
+
+
+@default_data.get('/retrieve-default-data')
+@jwt_required(refresh=True)
+def retrieve_module():
+    module_data = []
+    currency_data = []
     languages = []
     try:
+        module_default_data = db.session.query(
+            Modules.name, Modules.id).all()
+        for module in module_default_data:
+            module_data.append({
+                **module_schema.dump(module)
+            })
+
         language_default_data = db.session.query(
             Language.name, Language.id).all()
         for language in language_default_data:
@@ -57,17 +97,6 @@ def retrieve_languages():
                 **language_schema.dump(language),
             })
 
-        return Response.success(message="success", data=languages)
-
-    except Exception as e:
-        return response_with(resp.BAD_REQUEST_400)
-
-
-@default_data.get('/retrieve-currencies')
-@jwt_required(refresh=True)
-def retrieve_currencies():
-    currency_data = []
-    try:
         currency_default_data = db.session.query(
             Currency.code, Currency.id).all()
         for currency in currency_default_data:
@@ -75,7 +104,13 @@ def retrieve_currencies():
                 **currency_schema.dump(currency)
             })
 
-        return Response.success(message="success", data=currency_data)
+        default_data = {
+            "module": module_data,
+            "currency_data": currency_data,
+            "languages": languages
+        }
+
+        return Response.success(message="success", data=default_data)
 
     except Exception as e:
         print(e)
