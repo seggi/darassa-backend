ab1fe001ce077dc51204ac34145dc5d70c6b91ed
Seggi SMS <seggimarugira@gmail.com>
2022-09-19 16:28:32 +0200

    add some changes

--- api/admin/default/other_view.py
+++ api/admin/default/other_view.py
@@ -2,8 +2,8 @@ from this import s
 from flask import Blueprint
 from flask import request
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from api.database.model_marsh import ClassesSchema, LanguageSchema
-from api.database.models import Classes, Language
+from api.database.model_marsh import ClassesSchema, CurrencySchema, LanguageSchema
+from api.database.models import Classes, Currency, Language
 from ... import db
 from api.utils.responses import Response, response_with
 from api.utils import responses as resp
@@ -12,6 +12,7 @@ default_data = Blueprint("default_data", __name__,
                          url_prefix="/api/user/admin")
 
 language_schema = LanguageSchema()
+currency_schema = CurrencySchema()
 
 
 @default_data.post('/add-language')
@@ -49,7 +50,8 @@ def add_languages():
 def retrieve_languages():
     languages = []
     try:
-        language_default_data = db.session.query(Language.name).all()
+        language_default_data = db.session.query(
+            Language.name, Language.id).all()
         for language in language_default_data:
             languages.append({
                 **language_schema.dump(language),
@@ -58,4 +60,23 @@ def retrieve_languages():
         return Response.success(message="success", data=languages)
 
     except Exception as e:
+        return response_with(resp.BAD_REQUEST_400)
+
+
+@default_data.get('/retrieve-currencies')
+@jwt_required(refresh=True)
+def retrieve_currencies():
+    currency_data = []
+    try:
+        currency_default_data = db.session.query(
+            Currency.code, Currency.id).all()
+        for currency in currency_default_data:
+            currency_data.append({
+                **currency_schema.dump(currency)
+            })
+
+        return Response.success(message="success", data=currency_data)
+
+    except Exception as e:
+        print(e)
         return response_with(resp.BAD_REQUEST_400)
